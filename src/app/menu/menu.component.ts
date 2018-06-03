5df252a429a9a5d2504bbba00607f84a756cb812
Bus42 <gbrewton1980@gmail.com>
2018-06-02 21:45:33 -0500

    WIP gets font picker working, background menu not working...

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,6 +10,7 @@ export class MenuComponent implements OnInit {
   hideMenu() {
     let menuEl = document.getElementById('menuWrapper');
     let bgList = document.getElementById('bgList');
+    let fontList = document.getElementById('fontList');
     menuEl.classList.remove('menuActive');
     menuEl.classList.add('menuInactive');
     if (bgList.classList.contains('bgMenu-active')) {
@@ -27,12 +28,29 @@ export class MenuComponent implements OnInit {
   hideBgList() {
     let bgList = document.getElementById('bgList');
     bgList.classList.remove('bgMenu-active');
-      bgList.classList.add('bgMenu-inactive');
+    bgList.classList.add('bgMenu-inactive');
   }
 
   changeBg(bg) {
     document.body.style.background = `url(../assets/${bg})`;
   }
+
+  showFontList() {
+    let fontList = document.getElementById('fontList');
+    fontList.classList.remove('fontMenu-inactive');
+    fontList.classList.add('fontMenu-active');
+  }
+
+  hideFontList(){
+    let fontlist = document.getElementById('fontList');
+    fontlist.classList.remove('fontMenu-active');
+    fontlist.classList.add('fontMenu-inactive');
+  }
+
+  changeFont(font) {
+    document.body.style.fontFamily = `${font}, cursive`;
+  }
+
 
   constructor() { }
 
