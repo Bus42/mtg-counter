b5ef736b69f49cbd021ef6f89a4a8448715df6d9
Bus42 <gbrewton1980@gmail.com>
2018-06-03 17:29:56 -0500

    Adds dice roller

--- src/app/dice/dice.component.ts
+++ src/app/dice/dice.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiceComponent implements OnInit {
 
+  randNum (min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  roll(){
+    let dice = document.getElementsByName('d6');
+
+    let numString = {
+      1 : 'one',
+      2 : 'two',
+      3 : 'three',
+      4 : 'four',
+      5 : 'five',
+      6 : 'six'
+    }
+
+    for (var i = 0; i < dice.length; i++) {
+      let current = dice[i];
+      let newNum = this.randNum(1,6);
+      current.className = `fas fa-dice-${numString[newNum]}`;
+    }
+    
+  }
+
   constructor() { }
 
   ngOnInit() {
